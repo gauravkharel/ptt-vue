2a124ac842ca1a5e65f4ef3cfab668641acc01b1
gauravkharel <kgaurav21@tbc.edu.np>
2022-03-30 21:15:55 +0545

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header hidden>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Initial Project</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <RouterLink class="nav-link" aria-current="page" to="/"
+                >Home</RouterLink
+              >
+            </li>
+            <li class="nav-item">
+              <RouterLink to="/signin" class="nav-link">SignIn</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink to="/signup" class="nav-link">SignUp</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink to="/ticketform" class="nav-link">Ticket</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<style>
+h2 {
+  font-size: large;
+}
+</style>
---
+++ src/views/SignInView.vue
@@ -0,0 +1,29 @@
+<template>
+  <form @submit.prevent="onClick">
+    <div class="row mb-3">
+      <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="inputEmail3" />
+      </div>
+    </div>
+    <div class="row mb-3">
+      <label for="inputPassword3" class="col-sm-2 col-form-label"
+        >Password</label
+      >
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="inputPassword3" />
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </form>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
